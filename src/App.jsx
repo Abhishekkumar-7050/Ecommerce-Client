7ccea949eb7513e214a563d48bbae6cff53fbcd6
Abhishekkumar-7050 <abhishekofficial7050>
2024-05-09 16:56:05 +0530

    Basic react App Ready to do build

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import React from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+      <div>
+        hello 
+      </div>
+       
+  )
+}
+
+export default App
